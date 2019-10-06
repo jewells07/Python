a741f068b943639b9ca691bd71b130ecf0879cac
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-06 10:16:25 +0530

    Update areaofcircle(Typecaste).py

--- areaofcircle(Typecaste).py
+++ areaofcircle(Typecaste).py
@@ -1,11 +1,4 @@
-pi=3.14
-print("enter the area of circle")
-circle=input()
-print(type(circle))
-area=int(circle)*pi
-print("The area is:",area)
 
-print("ANOTHER WAY TO FIND")
 from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " ,r, " is: " + str(pi * r**2))
