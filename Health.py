2e975f2ccf0d2103999c850ebc39397294727795
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-10 22:42:43 +0530

    Update Health.py

--- Health.py
+++ Health.py
@@ -1,39 +1,49 @@
+import datetime
+def gettime():
+    return datetime.datetime.now()
 def take(k):
     if k==1:
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
-            with open("salman_food.txt","r+")as s:
+            with open("salman_food.txt","a")as s:
                 write=input("Write for Lock for Salman_food\n")
-                s.write(write)
+                s.write(str([str(gettime())])+": "+write+"\n")
+                print("successfully written")
+    
         elif c==2:
-            with open("salman_exercise.txt","r+")as s:
+            with open("salman_exercise.txt","a")as s:
                 write=input("Write for Lock for Salman_exercise\n")
-                s.write(write)
+                s.write(str([str(gettime())])+": "+write+"\n")
+                print("successfully written")
         else:
             print("Not Valid")
                 
     elif k==2:
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
-            with open("sharukh_food.txt","r+")as sk:
+            with open("sharukh_food.txt","a")as sk:
                 write=input("Write for Lock for Sharukh_food.txt\n")
-                sk.write(write)
+                sk.write(str([str(gettime())])+": "+write+"\n")
+                print("successfully written")
         elif c==2:
-            with open("sharukh_exercise.txt","r+")as sk:
+            with open("sharukh_exercise.txt","a")as sk:
                 write=input("Write for Lock for Sharukh_exercise\n")
-                sk.write(write)
+                sk.write(str([str(gettime())])+": "+write+"\n")
+                print("successfully written")
         else:
             print("Not Valid")
     elif k==3:
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
-            with open("aamir_food.txt","r+")as aa:
+            with open("aamir_food.txt","a")as aa:
                 write=input("Write for Lock for Aamir_food.txt\n")
-                aa.write(write)
+                aa.write(str([str(gettime())])+": "+write+"\n")
+                print("successfully written")
         elif c==2:
-            with open("aamir_exercise.txt","r+")as aa:
+            with open("aamir_exercise.txt","a")as aa:
                 write=input("Write for Lock for aamir_exercise\n")
-                aa.write(write)
+                aa.write(str([str(gettime())])+": "+write+"\n")
+                print("successfully written")
         else:
             print("Not Valid")
     else:
@@ -47,11 +57,11 @@ def retrieve(k):
     if k==1:
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
-            with open("salman_food.txt","r+")as s:
+            with open("salman_food.txt")as s:
                 for lines in s: 
                     print(lines)
         elif c==2:
-            with open("salman_exercise.txt","r+")as s:
+            with open("salman_exercise.txt")as s:
                 for lines in s: 
                     print(lines)
             
@@ -60,11 +70,11 @@ def retrieve(k):
     elif k==2:
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
-            with open("sharukh_food.txt","r+")as sk:
+            with open("sharukh_food.txt")as sk:
                 for lines in sk: 
                     print(lines)
         elif c==2:
-            with open("sharukh_exercise.txt","r+")as sk:
+            with open("sharukh_exercise.txt")as sk:
                 for lines in sk: 
                     print(lines)
         else:
@@ -72,11 +82,11 @@ def retrieve(k):
     elif k==3:
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
-            with open("aamir_foodf.txt","r+")as ak:
+            with open("aamir_foodf.txt")as ak:
                 for lines in ak: 
                     print(lines)
         elif c==2:
-            with open("aamir_exercise.txt","r+")as ak:
+            with open("aamir_exercise.txt")as ak:
                 for lines in ak: 
                     print(lines)
         else:
