ac25f14ec1c8e753688fc752e2e95206924aff87
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-10 20:50:24 +0530

    Update tell()&seek().py

--- tell()&seek().py
+++ tell()&seek().py
@@ -1,9 +1,9 @@
-f=open("File.txt","r+")
-print(f.readline())
-print(f.tell())                 #f=file pointer tell use where it is
 
-f.seek(10)                      #f=file pointer we set at the point 10
-
-print(f.tell())
-print(f.readline())
-f.close()
+with open("File.txt")as f:          #It will open and close file in this block:
+    print(f.readline())
+    print(f.tell())                 #f=file pointer tell use where it is
+    
+    f.seek(10)                      #f=file pointer we set at the point 10
+    
+    print(f.tell())
+    print(f.readline())
