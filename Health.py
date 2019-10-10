1e4ac6e4e27cfcf28a04b4710e284e01cbd74f54
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-10 22:25:25 +0530

    Update Health.py

--- Health.py
+++ Health.py
@@ -19,25 +19,25 @@ def take(k):
                 write=input("Write for Lock for Sharukh_food.txt\n")
                 sk.write(write)
         elif c==2:
-            with open("sharukh_exercise.txt","r+")as s:
+            with open("sharukh_exercise.txt","r+")as sk:
                 write=input("Write for Lock for Sharukh_exercise\n")
-                s.write(write)
+                sk.write(write)
         else:
             print("Not Valid")
     elif k==3:
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
-            with open("aamir_food.txt","r+")as a:
+            with open("aamir_food.txt","r+")as aa:
                 write=input("Write for Lock for Aamir_food.txt\n")
-                a.write(write)
-         elif c==2:
-            with open("aamir_exercise.txt","r+")as s:
+                aa.write(write)
+        elif c==2:
+            with open("aamir_exercise.txt","r+")as aa:
                 write=input("Write for Lock for aamir_exercise\n")
-                s.write(write)
+                aa.write(write)
         else:
             print("Not Valid")
     else:
-        print("PLEASE ENTER VALID OPTION:")
+        print("PLEASE ENTER VALID OPTION FOR PERSON:")
 
 
 
@@ -45,17 +45,42 @@ def take(k):
         
 def retrieve(k):
     if k==1:
-        with open("salman_exercise.txt","r+")as s:
-            for line in f: 
-                print(lines)
+        c=int(input("Enter 1(food) and 2(exercise)"))
+        if c==1:
+            with open("salman_food.txt","r+")as s:
+                for line in s: 
+                    print(lines)
+        elif c==2:
+            with open("salman_exercise.txt","r+")as s:
+                for line in s: 
+                    print(lines)
+            
+        else:
+            print("Not valid")
     elif k==2:
-        with open("sharukh_exercise.txt","r+")as s:
-            for line in f: 
-                print(lines)
+        c=int(input("Enter 1(food) and 2(exercise)"))
+        if c==1:
+            with open("sharukh_food.txt","r+")as sk:
+                for line in sk: 
+                    print(lines)
+        elif c==2:
+            with open("sharukh_exercise.txt","r+")as sk:
+                for line in sk: 
+                    print(lines)
+        else:
+            print("Not valid")
     elif k==3:
-        with open("aamir_exercise.txt","r+")as s:
-            for line in f: 
-                print(lines)
+        c=int(input("Enter 1(food) and 2(exercise)"))
+        if c==1:
+            with open("aamir_foodf.txt","r+")as ak:
+                for line in ak: 
+                    print(lines)
+        elif c==2:
+            with open("aamir_exercise.txt","r+")as ak:
+                for line in ak: 
+                    print(lines)
+        else:
+            print("Not valid")
     else:
         print("PLEASE ENTER VALID OPTION:")
     
@@ -73,8 +98,8 @@ a=int(input("Enter 1 for lock and 2 for retrieve"))
 if a==1:
     b=int(input("Enter 1(Salman) 2(Sharukh) 3(Aamir)"))
     take(b)
-# elif a==2:
-#     b=int(input("Enter 1(Salman) 2(Sharukh) 3(Aamir)"))
-#     retrieve(b)
+elif a==2:
+    b=int(input("Enter 1(Salman) 2(Sharukh) 3(Aamir)"))
+    retrieve(b)
 else:
     print("In Valid Option:")
