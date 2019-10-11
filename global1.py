dd9396823e1cb33f57ecec32907bcc095901b76f
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-11 17:38:37 +0530

    Create global1.py

---
+++ global1.py
@@ -0,0 +1,11 @@
+def jewells():
+    x=10
+    def jackie():
+        global x
+        x=20
+    print("Before callig jackie",x)
+    jackie()
+    print("After callig jackie",x)
+
+jewells()
+print(x)
