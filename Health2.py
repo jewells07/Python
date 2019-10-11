9a3006fd9c007a9518b06c8114109b95f024672b
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-11 22:42:15 +0530

    Update Health2.py

--- Health2.py
+++ Health2.py
@@ -1,3 +1,8 @@
+import sys          #For sys.exit() in password function
+import datetime
+def gettime():
+    return datetime.datetime.now()
+
 def Password(pas,k):
     """This Function is for Password:"""
     Dict = {1: 'Salman@123', 2: 'Sharukh@123', 3: 'Aamir@123'} 
@@ -10,43 +15,59 @@ def Password(pas,k):
         else:
                 #(EXAMPLE):1(Salman) == Sharukh@123(pass) WRONG
             sys.exit("WRONG Password")
+    else:
+        print("NOT IN OUR DATABASE:")
 
-import sys          #For sys.exit() in password function
-import datetime
-def gettime():
-    return datetime.datetime.now()
 
 client_list = {1:"Salman", 2:"Sharukh", 3:"Aamir"}
 lock_list = {1:"Exercise", 2:"Diet"}
 print("Select Client")
+
+#It will print the names and numbers from both List
 for key,value in client_list.items():
     print("Press",key,"for",value)
+    
 client_num=int(input())
 print("Selected",client_list[client_num],"\n")
 sel=int(input("Press 1(Lock) and 2(Retrieve)"))
+
+#FOR Lock----
 if sel is 1:
     pas=input("Enter the password:")
     Password(pas,client_num)
     c=int(input("Press 1(Exercise) 2(Diet)\n"))
+    
+    #FOR Exercise------ 
     if c==1:
         with open(client_list[client_num]+"_"+lock_list[c]+"txt","a")as w:
+            #(EXAMPLE)--write("Salman_Exercise.txt","a")as w
             write=input("Write for"+client_list[client_num]+"\n")
+            #client_list[client_num]=client_list[1]=Salman
+            
             w.write(str([str(gettime())])+": "+write+"\n")
+            #for date and time
             print("successfully written")
+    
+    #FOR Diet-----
     elif c==2:
         with open(client_list[client_num]+"_"+lock_list[c]+"txt","a")as w:
+            #(EXAMPLE)--write("Salman_Diet.txt","a")as w
             write=input("Write for "+client_list[client_num]+"\n")
+            #client_list[client_num]=client_list[1]=Salman
             w.write(str([str(gettime())])+": "+write+"\n")
             print("successfully written")
     else:
         sys.exit("WRONG INPUT")
-        
+ 
+#Retrieve-------       
 elif sel is 2:
     c=int(input("Press 1(Exercise) 2(Diet)\n"))
+    #FOR Exercise--------
     if c==1:
         with open(client_list[client_num]+"_"+lock_list[c]+"txt")as r:
             for lines in r: 
                 print(lines)
+    #FOR Diet-----
     elif sel==2:
          with open(client_list[client_num]+"_"+lock_list[c]+"txt")as r:
             for lines in r: 
