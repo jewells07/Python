1a45f8890f675fc3eba6aec51d1564e5167447e9
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-11 11:29:15 +0530

    Update Health.py

--- Health.py
+++ Health.py
@@ -7,17 +7,20 @@ def take(k):
     #FOR SALAMAN
     if k==1:
         pas=input("Enter your password:")
+        #For Password
         Password(pas,k)
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
             with open("salman_food.txt","a")as s:
                 write=input("Write for Lock for Salman_food\n")
+                #For date and time
                 s.write(str([str(gettime())])+": "+write+"\n")
                 print("successfully written")
     
         elif c==2:
             with open("salman_exercise.txt","a")as s:
                 write=input("Write for Lock for Salman_exercise\n")
+                #For datet and ime
                 s.write(str([str(gettime())])+": "+write+"\n")
                 print("successfully written")
         else:
@@ -26,16 +29,19 @@ def take(k):
     #FOR SHARUKH
     elif k==2:
         pas=input("Enter your password:")
+        #For Password
         Password(pas,k)
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
             with open("sharukh_food.txt","a")as sk:
                 write=input("Write for Lock for Sharukh_food.txt\n")
+                #For date and time
                 sk.write(str([str(gettime())])+": "+write+"\n")
                 print("successfully written")
         elif c==2:
             with open("sharukh_exercise.txt","a")as sk:
                 write=input("Write for Lock for Sharukh_exercise\n")
+                #For date and time
                 sk.write(str([str(gettime())])+": "+write+"\n")
                 print("successfully written")
         else:
@@ -44,16 +50,19 @@ def take(k):
     #FOR AAMIR
     elif k==3:
         pas=input("Enter your password:")
+        #For Password
         Password(pas,k)
         c=int(input("Enter 1(food) and 2(exercise)"))
         if c==1:
             with open("aamir_food.txt","a")as aa:
                 write=input("Write for Lock for Aamir_food.txt\n")
+                #For date and time
                 aa.write(str([str(gettime())])+": "+write+"\n")
                 print("successfully written")
         elif c==2:
             with open("aamir_exercise.txt","a")as aa:
                 write=input("Write for Lock for aamir_exercise\n")
+                #For date and time
                 aa.write(str([str(gettime())])+": "+write+"\n")
                 print("successfully written")
         else:
@@ -117,7 +126,10 @@ def retrieve(k):
 def Password(pas,k):
     """This Function is for Password:"""
     Dict = {1: 'Salman@123', 2: 'Sharukh@123', 3: 'Aamir@123'} 
+    #It takes the key(1,2,3,..) and match in Dictionary(Dict):
     if k in Dict.keys():
+        #if Key match with password then Granted
+        #(EXAMPLE): 1 (salman) == Salman@123(Password)
         if Dict.get(k)==pas:
             print("Password Granted")
         else:
@@ -142,4 +154,4 @@ try:
         print("In Valid Option:")
 except Exception as e:
     print("ERROR--404")
-    print("You Press Worng key or you didn't locked")
+    print("You Press Worng key or you didn't lock")
