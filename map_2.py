c36ca90c0f12949827c13aa632832d856e02ef5a
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-13 13:35:13 +0530

    Update map_2.py

--- map_2.py
+++ map_2.py
@@ -3,8 +3,12 @@ def square(a):
 def cube(a):
     return a*a*a
     
-fun=[square,cube]
+fun=[square,cube] #Having two functions square and cube::
 for i in range(5):
     val=list(map(lambda x:x(i),fun))
     print(val)
 #It will take fun variable map into lamba function that returns x(i)
+
+#Explaination of val=list(map(lambda x:x(i),fun))
+#lamba is the function take one function and return that value
+#EXAMPLE-- lamda take square function and return x(i)= x
