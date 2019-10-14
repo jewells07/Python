1a7191b56e337bc2cea7c33ef0dc00bbc30a2db5
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-14 18:13:09 +0530

    Delete enterNumber<100.py

--- enterNumber<100.py
+++
@@ -1,8 +0,0 @@
-#execute loop until your input is less than 100 
-while(True):
-    inp=int(input("Enter the number"))
-    if inp>100:
-        print("its is more than 100")
-        break
-    else:
-        continue
