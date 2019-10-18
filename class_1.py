ee68966cca9d0788bd02ae6b35ee5c286c616367
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-18 20:13:55 +0530

    Create class_1.py

---
+++ class_1.py
@@ -0,0 +1,12 @@
+class students:
+    pass
+
+rohan=students()
+amar=students()
+rohan.std=9
+rohan.subjects=["Phy","Maths","Chem."]
+
+amar.section='A'
+amar.std=10
+print(rohan.std,rohan.subjects)
+print(amar.section,amar.std)
