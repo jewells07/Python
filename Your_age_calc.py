2a6e80c1784208843e5c1006c1ad4b9a9f197871
jewells07 <54440548+jewells07@users.noreply.github.com>
2019-10-31 09:45:17 +0530

    Update Your_age_calc.py

--- Your_age_calc.py
+++ Your_age_calc.py
@@ -3,11 +3,11 @@ input1=input("Enter you age or birth year\n")
 length = len(input1)
 # print(length)
 try:
-    if int(input1) > 1919 and int(input1) < 2019 and length == 4:
+    if int(input1) > 1900 and int(input1) < 2019 and length == 4:
         print(f"You will be 100 at {int(input1) + 100}")
-    elif int(input1) < 99 and int(input1) >14 and length == 2:
+    elif int(input1) < 99 and int(input1) >0 and length == 2:
         print(f"You will be 100 at year{(2019 - int(input1)) + 100} ")
-    elif (int(input1) < 1919 and int(input1) > 99 ) or (length == 4 or length == 2):
+    elif (int(input1) < 1900 and int(input1) > 99 ) and (length == 4 or length == 2):
         print("You seem to be the oldest person alive ")
     elif int(input1) >2019 and length == 4:
         print("You are not yet born")
@@ -17,7 +17,7 @@ try:
     length2 = len(input2)
     if length == 4 and length2 == 4:
         print(f"You will be {int(input2) - int(input1)} at year {int(input2)}")
-    elif length == 2 and length2 == 4:
+    elif int(input2)>2019 and length == 2 and length2 == 4:
         birth_year = 2019 - int(input1)
         age = int(input2) - birth_year
         print(f"You will be {age} at {int(input2)} ")
